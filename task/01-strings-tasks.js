495b5c0a1784b37531c2e01307b7729543030011
rogoff <rogoff13@gmail.com>
2016-03-11 12:21:47 +0300

    getFirstChar

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -85,7 +85,7 @@ function extractNameFromTemplate(value) {
  *   'cat'       => 'c'
  */
 function getFirstChar(value) {
-    throw new Error('Not implemented');
+    return value.charAt(0);
 }
 
 /**
