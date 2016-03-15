3f40238062dacb302e57a06dab03b9508620a1d6
rogoff <rogoff13@gmail.com>
2016-03-16 01:51:25 +0300

    toNumber

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -227,7 +227,7 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-    throw new Error('Not implemented');
+    return Number.isNaN(Number(value)) ? def : Number(value);
 }
 
 module.exports = {
