520bab2b438ac0459c2060dc6b6f9562e4e357af
rogoff <rogoff13@gmail.com>
2016-03-16 01:44:36 +0300

    isPrime

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -203,7 +203,12 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-    throw new Error('Not implemented');
+    var i;
+    if (n < 4) return true;
+    for (i = 2; i <= Math.sqrt(n); i++) {
+        if (((n % i) === 0)) return false;
+    }
+    return true;
 }
 
 /**
