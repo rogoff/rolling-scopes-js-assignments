1b1c53225b73e251cdb87402503c4fba23c3a03f
rogoff <rogoff13@gmail.com>
2016-03-18 01:07:59 +0300

    Fix #4 issue getDistanceBetweenPoints

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -73,7 +73,7 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-    return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+    return Math.hypot(x2 - x1, y2 - y1);
 }
 
 /**
