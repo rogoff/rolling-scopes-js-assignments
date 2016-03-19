7b5fc814765d8cca4ccedd439857359f216e34fb
rogoff <rogoff13@gmail.com>
2016-03-20 02:00:49 +0300

    getStringsLength

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -131,7 +131,7 @@ function getUpperCaseStrings(arr) {
  *    [ 'angular', 'react', 'ember' ] => [ 7, 5, 5 ]
  */
 function getStringsLength(arr) {
-   throw new Error('Not implemented');
+   return arr.map(elem => elem.length);
 }
 
 /**
