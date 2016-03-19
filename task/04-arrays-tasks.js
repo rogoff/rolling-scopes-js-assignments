f3dd36157aace7f3199e6f3b76f553376c0d228d
rogoff <rogoff13@gmail.com>
2016-03-20 02:26:08 +0300

    toArrayOfSquares

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -217,7 +217,7 @@ function toCsvText(arr) {
  *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
  */
 function toArrayOfSquares(arr) {
-   throw new Error('Not implemented');
+   return arr.map(elem => elem * elem);
 }
 
 
