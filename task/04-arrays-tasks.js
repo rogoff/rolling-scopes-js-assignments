0d231aac802bf5e22368b7f08d60315b8e4c5909
rogoff <rogoff13@gmail.com>
2016-03-20 20:25:54 +0300

    selectMany

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -524,7 +524,7 @@ function group(array, keySelector, valueSelector) {
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
 function selectMany(arr, childrenSelector) {
-    throw new Error('Not implemented');
+   return arr.reduce((prev, curr) => prev.concat(childrenSelector(curr)), []);
 }
 
 
