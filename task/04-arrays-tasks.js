d4bb65b9520dcced99ce44d41ce4c9fd19dc2eb5
rogoff <rogoff13@gmail.com>
2016-03-20 15:01:25 +0300

    getFalsyValuesCount

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -355,7 +355,7 @@ function getItemsSum(arr) {
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
 function getFalsyValuesCount(arr) {
-   throw new Error('Not implemented');
+   return arr.reduce((prev, curr) => { if (!curr) prev++; return prev }, 0);
 }
 
 /**
