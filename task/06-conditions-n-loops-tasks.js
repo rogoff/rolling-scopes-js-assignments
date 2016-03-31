c0676329ef902856c4f09742688192badef9ebcf
rogoff <rogoff13@gmail.com>
2016-03-31 23:20:15 +0300

    doRectanglesOverlap

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -127,7 +127,8 @@ function isTriangle(a,b,c) {
  *  
  */
 function doRectanglesOverlap(rect1, rect2) {
-    throw new Error('Not implemented');
+    return (!(((rect1.left + rect1.width) < rect2.left) || ((rect2.left + rect2.width) < rect1.left)) &&
+            !(((rect1.top + rect1.height) < rect2.top) || ((rect2.top + rect2.height) < rect1.top)))
 }
 
 
